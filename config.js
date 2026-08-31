45b59e6837d3de7b4774dca5f7d253b505e40a79
NovaFadillah <nova.risqy63@smk.belajar.id>
2026-08-31 23:16:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
 // JANGAN gunakan service_role / secret key di frontend.
 window.APP_CONFIG = {
   SUPABASE_ENABLED: true,
-  SUPABASE_URL: "https://vijujeyhttorxokqdkds.supabase.co",
-  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpanVqZXlodHRvcnhva3Fka2RzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxODQ1NzMsImV4cCI6MjEwMzc2MDU3M30.8_4DqYTxXRYBzeaGTR6ZNUN6du4zYxNGO87x5E5QkfE"
+  SUPABASE_URL: "https://vnpiffxoplpyuaqttqhd.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZucGlmZnhvcGxweXVhcXR0cWhkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxODQ3NjQsImV4cCI6MjEwMzc2MDc2NH0.UnBv6IpVx82lOFHWrkZwG5mKHewZHWlGN7F2oOdJhg4"
 };
